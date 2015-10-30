fad37ed99b1cbad54e6797c01d639f3b0f9d2a99
Jason Arnold <jason@thejasonfile.com>
2015-10-29 22:44:53 -0400

    testing API

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var URL1 = "http://api.wunderground.com/api/f6707df2a16e713d/conditions/q/NY/New_York.json"
-function displayWeather(data) {
+function currentWeather(data) {
 	var weatherHTML = "<p>Conditions for " + data.current_observation.display_location.city + " </p>";
 	weatherHTML += "<p>" + data.current_observation.temperature_string + "</p>";
 	weatherHTML += "<p>Feels like " + data.current_observation.feelslike_string + "</p>"
@@ -8,6 +8,9 @@ function displayWeather(data) {
 }
 
 var URL2 = "http://api.wunderground.com/api/f6707df2a16e713d/forecast/q/NY/New_York.json"
+function forecastWeather(data) {
+	var forecastHTML = 
+}
 
-$.getJSON(URL1, displayWeather);
+$.getJSON(URL1, currentWeather);
 $.getJSON(URL2, displayWeather);
