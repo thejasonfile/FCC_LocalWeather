88f63539dfd77f131f86e028e854a20873a31cd0
Jason Arnold <jason@thejasonfile.com>
2015-10-30 15:23:39 -0400

    wrote HTML for weatherimage and temps divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-var URL1 = "http://api.wunderground.com/api/f6707df2a16e713d/conditions/q/NY/New_York.json"
+var URL = "http://api.wunderground.com/api/f6707df2a16e713d/geolookup/conditions/forecast/q/NY/New_York.json"
 function currentWeather(data) {
-	var weatherHTML = "<p>Conditions for " + data.current_observation.display_location.city + " </p>";
-	weatherHTML += "<p>" + data.current_observation.temperature_string + "</p>";
-	weatherHTML += "<p>Feels like " + data.current_observation.feelslike_string + "</p>"
-	weatherHTML += "<img src=" + data.current_observation.icon_url + ">";
+	var currentConditions = data.current_observation;
+	var forecast = data.forecast.simpleforecast.forecastday;
+	var location = currentConditions.display_location.full;
+	var weather = currentConditions.weather;
+	var weatherGif = "<img class='weatherimage' src='http://icons.wxug.com/i/c/k/clear.gif'></img>";
+	var temp = currentConditions.temp_f;
+	var feelsLike = currentConditions.feelslike_f;
+	var precip = currentConditions.precip_today_in;
+
+	var weatherHTML = "<p>Current Conditions for<br>" + location + "</p>";
+	weatherHTML += weatherGif + "</br>";
+	weatherHTML += "<p>" + weather + "</p>";
+
+	var forecastHTML = "<p>" + temp + "</p>";
+	forecastHTML += "<p>Feels like: " + feelsLike;
+	forecastHTML += "<p>Hi: " + forecast[0].high.fahrenheit;
+	forecastHTML += "Lo: " + forecast[0].low.fahrenheit + "</p>";
+	forecastHTML += "<p>Precip Today: " + precip + "</p>";
+
 	$("#weatherimage").html(weatherHTML);
+	$("#temps").html(forecastHTML);
 }
 
-var URL2 = "http://api.wunderground.com/api/f6707df2a16e713d/forecast/q/NY/New_York.json"
-function forecastWeather(data) {
-	var forecastHTML = 
-}
-
-$.getJSON(URL1, currentWeather);
-$.getJSON(URL2, displayWeather);
+$.getJSON(URL, currentWeather);
