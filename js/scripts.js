4e9640bba2686b4460219fbabcc99a30a048a605
Jason Arnold <jason@thejasonfile.com>
2015-10-30 15:46:56 -0400

    added HTML for forecast days, JS for 1st forcast day

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,21 @@ function currentWeather(data) {
 
 	var forecastHTML = "<p>" + temp + "</p>";
 	forecastHTML += "<p>Feels like: " + feelsLike;
-	forecastHTML += "<p>Hi: " + forecast[0].high.fahrenheit;
-	forecastHTML += "Lo: " + forecast[0].low.fahrenheit + "</p>";
+	forecastHTML += "<p>Lo: " + forecast[0].low.fahrenheit;
+	forecastHTML += "Hi: " + forecast[0].high.fahrenheit + "</p>";
 	forecastHTML += "<p>Precip Today: " + precip + "</p>";
+
+	//create loop to add this to all 3 forecast days
+	var forecastDay1HTML = "<p>" + forecast[1].date.weekday + "</p>";
+	forecastDay1HTML += "<p>" + forecast[1].conditions + "</p>";
+	forecastDay1HTML += "<p><img class='weatherimage' src='http://icons.wxug.com/i/c/k/partlycloudy.gif'></img>";
+	forecastDay1HTML += "<p>Lo: " + forecast[1].low.fahrenheit;
+	forecastDay1HTML += "<p>Hi: " + forecast[1].high.fahrenheit + "</p>";
+	forecastDay1HTML += "<p>Precip: " + forecast[1].qpf_allday.in + "</p>"
 
 	$("#weatherimage").html(weatherHTML);
 	$("#temps").html(forecastHTML);
+	$("#day1").html(forecastDay1HTML);
 }
 
 $.getJSON(URL, currentWeather);
